7d1f6d522194831a21d58385f972f8b1ff1a9462
tlatib <>
2019-01-19 16:34:47 -0800

    Paperbot ready for testing

--- Lab1/paperbot/Software/paperbot/paperbot.ino
+++ Lab1/paperbot/Software/paperbot/paperbot.ino
@@ -48,7 +48,28 @@
 #include "file.h"
 #include "server.h"
 
+//sensors:
+#include <Wire.h>
+#include <VL53L0X.h>
 
+#define SDA_PORT 14
+#define SCL_PORT 12
+VL53L0X sensor;
+VL53L0X sensor2;
+
+#define    MPU9250_ADDRESS            0x68
+#define    MAG_ADDRESS                0x0C
+
+#define    GYRO_FULL_SCALE_250_DPS    0x00  
+#define    GYRO_FULL_SCALE_500_DPS    0x08
+#define    GYRO_FULL_SCALE_1000_DPS   0x10
+#define    GYRO_FULL_SCALE_2000_DPS   0x18
+
+#define    ACC_FULL_SCALE_2_G        0x00  
+#define    ACC_FULL_SCALE_4_G        0x08
+#define    ACC_FULL_SCALE_8_G        0x10
+#define    ACC_FULL_SCALE_16_G       0x18
+//end sensors
 const int SERVO_LEFT = D1;
 const int SERVO_RIGHT = D2;
 Servo servo_left;
@@ -71,6 +92,36 @@ char* mDNS_name = "paperbot";
 
 String html;
 String css;
+
+// sensor functions:
+
+// This function read Nbytes bytes from I2C device at address Address. 
+// Put read bytes starting at register Register in the Data array. 
+void I2Cread(uint8_t Address, uint8_t Register, uint8_t Nbytes, uint8_t* Data)
+{
+  // Set register address
+  Wire.beginTransmission(Address);
+  Wire.write(Register);
+  Wire.endTransmission();
+  
+  // Read Nbytes
+  Wire.requestFrom(Address, Nbytes); 
+  uint8_t index=0;
+  while (Wire.available())
+    Data[index++]=Wire.read();
+}
+
+
+// Write a byte (Data) in device (Address) at register (Register)
+void I2CwriteByte(uint8_t Address, uint8_t Register, uint8_t Data)
+{
+  // Set register address
+  Wire.beginTransmission(Address);
+  Wire.write(Register);
+  Wire.write(Data);
+  Wire.endTransmission();
+}
+//end sensor functions
 
 void setup() {
     setupPins();
@@ -145,7 +196,6 @@ void right() {
 }
 
 
-
 //
 // Setup //
 //
@@ -162,8 +212,156 @@ void setupPins() {
     servo_left.attach(SERVO_LEFT);
     servo_right.attach(SERVO_RIGHT);
     DEBUG("Setup motor pins");
+    // Sensors: 
+    pinMode(D3, OUTPUT);
+    pinMode(D4, OUTPUT);
+    digitalWrite(D7, LOW);
+    digitalWrite(D8, LOW);
+  
+    delay(500);
+    Wire.begin(SDA_PORT,SCL_PORT);
+  
+    Serial.begin (115200);
+  
+    digitalWrite(D3, HIGH);
+    delay(150);
+    Serial.println("00");
+    
+    sensor.init(true);
+    Serial.println("01");
+    delay(100);
+    sensor.setAddress((uint8_t)22);
+  
+    digitalWrite(D4, HIGH);
+    delay(150);
+    sensor2.init(true);
+    Serial.println("03");
+    delay(100);
+    sensor2.setAddress((uint8_t)25);
+    Serial.println("04");
+  
+    Serial.println("addresses set");
+    
+    Serial.println ("I2C scanner. Scanning ...");
+    byte count = 0;
+  
+    for (byte i = 1; i < 120; i++)
+    {
+  
+      Wire.beginTransmission (i);
+      if (Wire.endTransmission () == 0)
+      {
+        Serial.print ("Found address: ");
+        Serial.print (i, DEC);
+        Serial.print (" (0x");
+        Serial.print (i, HEX);
+        Serial.println (")");
+        count++;
+        delay (1);  // maybe unneeded?
+      } // end of good response
+    } // end of for loop
+    Serial.println ("Done.");
+    Serial.print ("Found ");
+    Serial.print (count, DEC);
+    Serial.println (" device(s).");
+  
+    delay(3000);
+  
+    // Set by pass mode for the magnetometers
+    I2CwriteByte(MPU9250_ADDRESS,0x37,0x02);
+    
+    // Request first magnetometer single measurement
+    I2CwriteByte(MAG_ADDRESS,0x0A,0x01);
+    //end sensors
 }
 
+void sendMagnetometerData(){
+    // Request first magnetometer single measurement
+    I2CwriteByte(MAG_ADDRESS,0x0A,0x01);
+    
+    // Read register Status 1 and wait for the DRDY: Data Ready
+    
+    uint8_t ST1;
+    uint8_t ii = 0;
+    do
+    {
+      
+      Serial.print("  -- sensor broke :( \n");
+      I2Cread(MAG_ADDRESS,0x02,1,&ST1);
+    }
+    while (!(ST1&0x01));
+  
+    // Read magnetometer data  
+    uint8_t Mag[7];  
+    I2Cread(MAG_ADDRESS,0x03,7,Mag);
+  
+    // Create 16 bits values from 8 bits data
+    
+    // Magnetometer
+    int16_t mx=(Mag[1]<<8 | Mag[0]);
+    int16_t my=(Mag[3]<<8 | Mag[2]);
+    int16_t mz=(Mag[5]<<8 | Mag[4]);
+  
+    float heading = atan2(mx, my);
+  
+    // Once you have your heading, you must then add your 'Declination Angle',
+    // which is the 'Error' of the magnetic field in your location. Mine is 0.0404 
+    // Find yours here: http://www.magnetic-declination.com/
+    
+    // If you cannot find your Declination, comment out these two lines, your compass will be slightly off.
+    float declinationAngle = 0.0404;
+    heading += declinationAngle;
+  
+    // Correct for when signs are reversed.
+    if(heading < 0)
+      heading += 2*PI;
+  
+    // Check for wrap due to addition of declination.
+    if(heading > 2*PI)
+      heading -= 2*PI;
+  
+    // Convert radians to degrees for readability.
+    float headingDegrees = heading * 180/PI; 
+  
+    /*Serial.print("\rHeading:\t");
+    Serial.print(heading);
+    Serial.print(" Radians   \t");
+    Serial.print(headingDegrees);
+    Serial.println(" Degrees   \t");
+  
+    Serial.print ("Magnetometer readings:"); 
+    Serial.print ("\tMx:");
+    Serial.print (mx); 
+    Serial.print ("\tMy:");
+    Serial.print (my);
+    Serial.print ("\tMz:");
+    Serial.print (mz);  
+    Serial.println ("\t");*/
+    //Might need to adjust to lower size(?)
+    char tx[100] = "blank";
+    sprintf(tx, "Mx,My,Mz: (%d, %d, %d). Deg: (%f)\0", mx, my, mz, headingDegrees);
+    wsSend(id, tx);
+}
+
+void sendLidarData(){
+  /*Serial.print("Lidar 1 range(mm): ");
+  Serial.print(sensor.readRangeSingleMillimeters());
+  if (sensor.timeoutOccurred()) { Serial.print(" TIMEOUT"); }
+  
+  Serial.print("  Lidar 2 range(mm): ");
+  Serial.println(sensor2.readRangeSingleMillimeters());
+  if (sensor.timeoutOccurred()) { Serial.println(" TIMEOUT"); }*/
+  char tx[100] = "blank";
+  if (sensor.timeoutOccurred() || sensor2.timeoutOccurred()){
+    sprintf(tx, "TIMEOUT");
+  }
+  else {
+    //Maybe these are floats? idk
+    sprintf(tx, "L1 (mm), L2(mm): (%d,%d)", sensor.readRangeSingleMillimeters(), sensor2.readRangeSingleMillimeters());
+  }
+    wsSend(id, tx);
+
+}
 void webSocketEvent(uint8_t id, WStype_t type, uint8_t * payload, size_t length) {
 
     switch(type) {
@@ -197,7 +395,7 @@ void webSocketEvent(uint8_t id, WStype_t type, uint8_t * payload, size_t length)
             if (payload[0] == '#') {
                 if(payload[1] == 'C') {
                   LED_ON;
-                  wsSend(id, "Hello world!");
+                  wsSend(id, "Hello world! Testing send data");
                 }
                 else if(payload[1] == 'F') 
                   forward();
@@ -215,6 +413,8 @@ void webSocketEvent(uint8_t id, WStype_t type, uint8_t * payload, size_t length)
                   char tx[20] = "Zero @ (xxx, xxx)";
                   sprintf(tx, "Zero @ (%3d, %3d)", servo_left_ctr, servo_right_ctr);
                   wsSend(id, tx);
+                  sendMagnetometerData();
+                  sendLidarData();
                 }
                 else if(payload[1] == 'D') {
                   if(payload[2] == 'L') 
@@ -224,6 +424,8 @@ void webSocketEvent(uint8_t id, WStype_t type, uint8_t * payload, size_t length)
                   char tx[20] = "Zero @ (xxx, xxx)";
                   sprintf(tx, "Zero @ (%3d, %3d)", servo_left_ctr, servo_right_ctr);
                   wsSend(id, tx);
+                  sendMagnetometerData();
+                  sendLidarData();
                 }
                 else 
                   stop();
