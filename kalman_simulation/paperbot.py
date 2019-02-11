9abdf54f1b3134a7a61e31fcab7c14b9f1197c97
aguscerdo <agus.marinovic@gmail.com>
2019-02-10 23:08:13 -0800

    Added lots of comments

--- kalman_simulation/paperbot.py
+++ kalman_simulation/paperbot.py
@@ -3,47 +3,66 @@ import matplotlib.pyplot as plt
 from matplotlib.patches import Ellipse
 
 class PaperBot:
+    """
+    Paperbot Class.
+    """
     def __init__(self, x0, y0, th0):
+        # Initialize state, x, y, th
         self.x = x0
         self.y = y0
         self.th = th0
+        
+        # History of states for plotting
         self.history = []
         
+        # Initialize measurements
         self.lidar_f = 0
         self.lidar_r = 0
         self.magnetometer = 0
 
+        # Step number
         self.t = 0
         
-        self.dt = 0.1
-        self.d = 49
-        self.A = 84
-
-        self.dims = [750, 500]
-        self.sensors = SensorEstimator(self.dims[0], self.dims[1])
-        self._add_history()
-        
+        # Multiplicative constants
+        self.dt = 0.1   # Time diff between updates
+        self.d = 49     # Wheel diameter
+        self.A = 84     # Axle length
         self.dconst = self.dt / 2 * np.pi * self.d
+
+        self.dims = [750, 500]  # Box dimensions
+        self.est_sensors = SensorEstimator(self.dims[0], self.dims[1])  # Estimates sensor measurements from state
+        self._add_history()     # Add entry 0 to history (x0, y0, th0)
         
         # Covariances
+        # Sensor cov
         self.R = np.zeros((3, 3))
         self.R[0, 0] = 3.28
         self.R[1, 1] = 1.86
         self.R[2, 2] = 0.8
 
-        # TODO improve this
+        # TODO put correct values
+        # Actuators cov
         self.Q = np.zeros((3, 3))
         self.Q[0, 0] = 0.25 * self.dt
         self.Q[1, 1] = 0.25 * self.dt
         self.Q[2, 2] = 0.89
         
+        # TODO initialize P properly
+        # Kalman cov
         self.P = self.Q.copy()
 
+        # Init jacobians to empty
         self.jacF = np.eye(3)
         self.jacH = np.eye(3)
     
     
     def _jacobian_H(self, min0=True, min1=True):    # Var to determine if min is l0 or l1
+        """
+        Populates jacobian of H given current state
+        :param min0: bool: indicates if the minimum distance was the first (1/cos) or second (1/sin) for front lidar
+        :param min1: bool: indicates if the minimum distance was the first (1/cos) or second (1/sin) for right lidar
+        :return:
+        """
         th0 = self.th
         th1 = self.th + np.pi / 2
         if th1 > 2 * np.pi:
@@ -51,6 +70,7 @@ class PaperBot:
         
         min_arr = [min0, min1]
         
+        # TODO fill up function. Ref is commented below
         for i, t in enumerate([th0, th1]):
             truth = min_arr[i]
             dy = self.dims[1] - self.y
@@ -70,7 +90,6 @@ class PaperBot:
             else:
                 pass
         
-
         # elif Th < self.p2 * 2:
         #     l0 = dx / coth
         #     l1 = Y / sith
@@ -81,11 +100,16 @@ class PaperBot:
         #     l0 = X / coth
         #     l1 = dy / sith
         
-        
         self.jacH[2, 2] = 1 / (1 + self.th ** 2)
 
 
     def _jacobian_F(self, wl, wr):
+        """
+        Populate jacobian of F given current state
+        :param wl: angular velocity of left wheel
+        :param wr: angular velocity of right wheel
+        :return:
+        """
         self.jacF[0, 0] = 1.
         self.jacF[0, 1] = 0.
         self.jacF[0, 2] = self.dconst * (wl + wr) * -self.ssin(self.th)
@@ -100,37 +124,73 @@ class PaperBot:
         
 
     def _add_history(self):
+        """
+        Add an entry to  history arr and increase time_step t
+        :return:
+        """
         self.history.append([self.x, self.y, self.th, self.t])
         self.t += 1
 
 
     def scos(self, th):
+        """
+        Performs magentic cos. This is equiv to cos(90 - theta). Magnetometer is with respect to y and clockwise
+        :param th: angle
+        :return:
+        """
         return np.cos(np.pi / 2 - th)
 
 
     def ssin(self, th):
+        """
+        Performs magentic sin. This is equiv to sin(90 - theta). Magnetometer is with respect to y and clockwise
+        :param th: angle
+        :return:
+        """
         return np.sin(np.pi / 2 - th)
 
 
     def move(self, ul, ur):
+        """
+        Entry point for user. Simulate getting input ul and ur
+        :param ul: int [0, 180]: input for left motor
+        :param ur: int [0, 180]: input for right motor
+        :return:
+        """
         print('t: {} -- X: {}, Y: {}, Th: {}'.format(self.t, self.x, self.y, self.th))
         print('\tul: {}, ur: {}'.format(ul, ur))
         ul -= 90
         ur -= 90
     
+        # Input to angular velocity
         wl = np.sign(ul) * np.power(np.abs(ul), 0.2)
         wr = np.sign(ur) * np.power(np.abs(ur), 0.2)
         print('\tw1: {}, w2: {}'.format(wl, wr))
     
-        self.apriori(wl, wr)
-        self.sense()
-        self._add_history()
+        self.apriori(wl, wr)    # Apriori state update
+        self.sense()            # Perform sensor measurements (simulate)
+        #self.posteriori()       # Perform posteriori update
+        self._add_history()     # Add current state
         
 
+    # TODO implement
     def sense(self):
+        """
+        Simulate lidar and magnetometer measurements
+        :return:
+        """
         pass
 
+
     def apriori(self, wl, wr):
+        """
+        Apriori state update. Updates state and covariance given input
+        :param wl: angular velocity of left wheel
+        :param wr: angular velocity of right wheel
+        :return:
+        """
+        
+        # Movement equations
         dx =  self.dconst * self.scos(self.th) * (wl + wr)
         dy = self.dconst * self.ssin(self.th) * (wl + wr)
         dth = self.dconst / self.A * (wl - wr)
@@ -144,19 +204,27 @@ class PaperBot:
         while self.th < 0:
             self.th += 2 * np.pi
         
+        # Update JacobianF and Kalman cov
         self._jacobian_F(wl, wr)
         self.P = self.jacF.dot(self.P.dot(self.jacF.T)) + self.Q
 
-            
-
+        
     def posteriori(self):
-        estimates = self.sensors.ret(self.x, self.y, self.th)
-        # TODO correctly get this stuff
+        """
+        Perform posteriori state update given sensor data
+        :return:
+        """
+        # Get estimated sensor measurements from state
+        estimates = self.est_sensors.ret(self.x, self.y, self.th)
+        # TODO I changed what estimates returns to account for min(x/cos, y/sin) type of thing for gradients
+            # TODO update accordingly
         
-        
+        # Calculate innovation and its cov
         innovation = np.asarray([self.lidar_f - est_F, self.lidar_r - est_R, self.magnetometer - est_M], float)
         innovation_cov = self.jacH.dot(self.P.dot(self.jacH.T)) + self.Q
         
+        
+        # Find inverse. TODO narrow down except clause
         flag = 1
         while flag:
             try:
@@ -164,13 +232,21 @@ class PaperBot:
                 flag = 0
             except:
                 innovation_cov += np.eye(innovation_cov.shape[0]) * 1e-5
-                Sinv = np.eye()
+                Sinv = np.eye(innovation_cov.shape[0])
         
+        # TODO finish off kalman update
         kalman = self.P.dot(self.jacH.T).dot(Sinv)
         
         
     
     def plot_history(self):
+        """
+        Plots movement over time and final step lidar projections.
+        Add any desired plottings here
+        :return:
+        """
+        
+        # Obtain x, y, th, t for every time t
         nparr = np.asarray(self.history)
     
         X = list(nparr[:, 0])
@@ -180,34 +256,39 @@ class PaperBot:
 
         fig, ax = plt.subplots(nrows=1, ncols=1)
         
-        ax.scatter(X, Y, c=t, cmap='RdBu')
-        # plt.colorbar(colormap)
-    
+        ax.scatter(X, Y, c=t, cmap='RdBu')  # Scatter position over time
+        
+        # Text
         ax.set_title('Position over Time: {} - {}'.format(t[-1], Th[-1]))
         ax.set_xlabel('X (mm)')
         ax.set_ylabel('Y (mm)')
         
-        mmin = np.minimum(np.min(X), np.min(Y))
-        mmax = np.maximum(np.max(X), np.max(Y))
-
+        # mmin = np.minimum(np.min(X), np.min(Y))
+        # mmax = np.maximum(np.max(X), np.max(Y))
         # ax.xlim([0, mmax + 250])
         # ax.ylim([0, mmax + 250])
-        ax.axis('equal')
-        ax.plot([0, self.dims[0], self.dims[0], 0, 0], [0, 0, self.dims[1], self.dims[1], 0], c='k')
+        
+        ax.axis('equal')    # Maintain ratio
+        ax.plot([0, self.dims[0], self.dims[0], 0, 0], [0, 0, self.dims[1], self.dims[1], 0], c='k')    # Plot box lines
 
-        self.sensors.plot_line(X[-1], Y[-1], Th[-1], ax)
-        ell = self.plot_ellipse(X[-1], Y[-1])
-        ax.add_artist(ell)
+        # Suplemental plotting
+        self.est_sensors.plot_line(X[-1], Y[-1], Th[-1], ax)    # Add lidar projection lines
+        ell = self.plot_ellipse()   # Covariance ellipse. Not currently working or ellipses are too big
+        ax.add_artist(ell)  # Add ellipse to fig
         
         plt.show()
 
     
-    def plot_ellipse(self, x, y):
+    def plot_ellipse(self):
+        """
+        Plot covariance ellipse centered at x, y
+        :return:
+        """
         cov = self.P[:2, :2]
         lambda_, v = np.linalg.eig(cov)
         lambda_ = np.sqrt(lambda_)
         
-        ell = Ellipse(xy=(x, y),
+        ell = Ellipse(xy=(self.x, self.y),
                       width=lambda_[0] * 2, height=lambda_[1] * 2,
                       angle=np.rad2deg(np.arccos(v[0, 0])))
         ell.set_facecolor('none')
@@ -215,6 +296,9 @@ class PaperBot:
         
 
 class SensorEstimator:
+    """
+    SensorEstimator class. Estimates sensor measurements given state
+    """
     def __init__(self, xdim, ydim):
         self.x = xdim
         self.y = ydim
@@ -223,10 +307,27 @@ class SensorEstimator:
 
 
     def ret(self, X, Y, Th):
-        return [self.front_lidar(X, Y, Th), self.right_lidar(X, Y, Th), Th + np.random.normal(0, 0.15)]
+        """
+        Returns sensor estimations given state
+        :param X: current X coordinate
+        :param Y: current Y coordinate
+        :param Th: current state angle
+        :return: list:
+                    0 - front lidar [a/cos, b/sin] returns both line projections. Only take min. Both needed for gradient
+                    1 - right lidar [a/cos, b/sin] returns both line projections. Only take min. Both needed for gradient
+                    2 - Angle estimate (which is angle) but with added noise
+        """
+        return [self.front_lidar(X, Y, Th), self.right_lidar(X, Y, Th), Th + np.random.normal(0, 0.15)] # TODO remove noise?
 
 
     def front_lidar(self, X, Y, Th):
+        """
+        Predict front lidar distance reading from state
+        :param X: current X coordinate
+        :param Y: current Y coordinate
+        :param Th: current state angle
+        :return: both lidar projected distances. Only use min. First one is always a/cos and second is always b/sin
+        """
         while Th > self.p:
             Th -= self.p
         while Th < 0:
@@ -259,21 +360,40 @@ class SensorEstimator:
 
 
     def right_lidar(self, X, Y, Th):
+        """
+        Right lidar projection. Is same formulation as front lidar but shifted 90 degrees
+        :param X: current X coordinate
+        :param Y: current Y coordinate
+        :param Th: current state angle
+        :return: both lidar projected distances (small and big). Only use min. First one is always a/cos and second is always b/sin
+        """
         return self.front_lidar(X, Y, Th + self.p2)
 
 
     def plot_line(self, X, Y, Th, ax):
+        """
+        Plot projected lines to walls for a given state
+        :param X: state X coordinate
+        :param Y: state Y coordinate
+        :param Th: state angle
+        :param ax: plotting axes
+        :return:
+        """
+        
+        # Get lines and use minimum
         lf0, lf1 = self.front_lidar(X, Y, Th)
         lf = min(lf0, lf1)
         lr0, lr1 = self.right_lidar(X, Y, Th)
         lr = min(lr0, lr1)
 
+        # Get endpoints
         p21X = X + np.cos(self.p2 - Th) * lf
         p21Y = Y + np.sin(self.p2 - Th) * lf
 
         p22X = X + np.cos(-Th) * lr
         p22Y = Y + np.sin(-Th) * lr
 
+        # Print endpoints
         ax.plot([X, p21X], [Y, p21Y],c='r', label='Front')
         ax.plot([X, p22X], [Y, p22Y],c='b', label='Right')
         ax.legend()
--- kalman_simulation/simulate.py
+++ kalman_simulation/simulate.py
@@ -1,23 +1,48 @@
 from paperbot import PaperBot
+# TODO fix dependency if needed with below
+# import sys
+# sys.path.append('./kalman_simulation')
 import numpy as np
 
 
 def main():
-	n = 15
-	n2 = 5
-	n3 = 5
-	ul = [180] * n + [0] * n2 + [0] * n3 + [100] * n3 + [0] * 2
-	ur = [100] * n + [0] * n2 + [180] * n3 + [100] * n3 + [180] * 2
+	"""
+	Performs a simulation session
+	:return:
+	"""
 	
-	p = PaperBot(100, 0, 0)
+	# Control different movements repeated n times
+	n1 = 30
+	v1L = 180
+	v1R = 95
+	
+	n2 = 5
+	v2L = 0
+	v2R = 0
+	
+	n3 = 20
+	v3L = 0
+	v3R = 90
+	
+	n4 = 20
+	v4L = 101
+	v4R = 99
+	
+	ul = [v1L] * n1 + [v2L] * n2 + [v3L] * n3 + [v4L] * n4
+	ur = [v1R] * n1 + [v2R] * n2 + [v3R] * n3 + [v4R] * n4
+	
+	# Initialize paperbot at X, Y, Th
+	p = PaperBot(100, 100, 0)
 	
 	for i in range(min(len(ul), len(ur))):
 		p.move(ul[i], ur[i])
 		
+		# Plot every 5
 		if i % 5 == 0:
 			p.plot_history()
 		# p.plot_history()
 
+	# Final plotting
 	p.plot_history()
 	
 	
