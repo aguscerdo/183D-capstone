03068d2c0946118acadef4b3337c7ea5598dbef0
lucidelectronics <drumpact@gmail.com>
2019-02-26 22:37:12 -0800

    Update MotionTrack.py

--- Lab4/MotionTrack.py
+++ Lab4/MotionTrack.py
@@ -12,7 +12,7 @@ from math import *
 cap = cv2.VideoCapture(0)
 
 #Set the HSV bounds for the two tags
-lower_blue = np.array([90, 100, 50])
+lower_blue = np.array([90, 100, 90])
 upper_blue = np.array([140, 255, 255])
 lower_white = np.array([0,0,200])
 upper_white = np.array([180,255,255])
