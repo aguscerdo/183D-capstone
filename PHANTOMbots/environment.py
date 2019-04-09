4b85e80a78e0b75ca01c3a0acbb8968af922a8d9
tluccs <tameezlatib@gmail.com>
2019-04-08 18:55:55 -0700

    Added phantomBot class, more environment features

--- PHANTOMbots/environment.py
+++ PHANTOMbots/environment.py
@@ -3,38 +3,47 @@
 #	vertices is the set of all vertices in our maze,
 # 	a vertice is defined by its x and y coordinate ([x, y])
 #	We assume that all adjacent vertices connected if they are in the grid
+#	bots is a list of PhantomBots in our simulation
 
 import matplotlib.pyplot as plt
 import numpy as np
+from phantomBot import PhantomBot
 # TODO: change these later
-presetSize = [3, 3]
-v1 = [0,0]
-v2 = [0,1]
-v3 = [1,0]
-v4 = [1,1]
-v5 = [2,0]
-v6 = [2,1]
-v7 = [2,2]
-presetVertices = [v1, v2, v4, v5, v6, v7]
+presetSize = [10, 10]
+presetVertices = []
+for i in range(presetSize[0]):
+	for j in range(presetSize[1]):
+		if (i !=j and (i != 6 or j > 5) and (j!=9 or i <2) ) or ((j == 6 or j == 2) and i == 6):
+			presetVertices.append([i,j])
 
 class Environment:
-	def __init__(self, printAll=False, size=None, vertices=None ):
+	def __init__(self, printAll=False, size=None, vertices=None, bots=None ):
 		self.printAll = printAll
 		if (self.printAll):
 			print("Print all mode on!")
-		if size is None or vertices is None:
+		if size is None or vertices is None or bots is None:
 			self.size = presetSize
 			self.vertices = presetVertices
+			pursuer1 = PhantomBot(printAll=False, pos=[0,1], pacman=False) 
+			pursuer2 = PhantomBot(printAll=False, pos=[0,5]) 
+			pursuer3 = PhantomBot(printAll=False, pos=[0,7]) 
+			pursuer4 = PhantomBot(printAll=False, pos=[5,1]) 
+			pacmanBot = PhantomBot(printAll=False, pos=[6,6], pacman=True, speed=1.2) 
+			self.bots = [pursuer1, pursuer2, pursuer3, pursuer4, pacmanBot]
 			if (self.printAll):
-				print("Using preset size/edges!")
+				print("Using preset size/edges/bots!")
 		else:
 			self.size = size
 			self.vertices = vertices
+			self.bots = bots
 			if (self.printAll):
 				print("Using grid of size " + str(self.size))
 
 		self.verticeMatrix = np.zeros(self.size)
 		self.set_vertice_matrix()
+		self.occupiedVertices = []
+		for bot in self.bots:
+			self.occupiedVertices.append(bot.get_position())
 
 	def set_vertice_matrix(self):
 		"""
@@ -62,23 +71,32 @@ class Environment:
 		"""
 		Plots the edges by plotting a line for each edge
 		"""
+		ax = plt.gca()
 		if (self.printAll):
 			print("Plotting Grid!")
 		for i in range(self.size[0]):
 			for j in range(self.size[1]):
 				#plot vertex i,j
 				if (self.verticeMatrix[i,j]):
-					plt.plot(i, j, 'bx', label='point')
+					ax.plot(i, j, 'bx', label='point')
 				#plot line from i,j -> i+1,j 
 				if (i+1 < self.size[0] and self.verticeMatrix[i,j] and self.verticeMatrix[i+1,j]):
 					xs = [i, i+1]
 					ys = [j, j]
-					plt.plot(xs, ys, 'r')
+					ax.plot(xs, ys, 'r')
 				#plot line from i,j -> i,j+1
 				if (j+1 < self.size[1] and self.verticeMatrix[i,j] and self.verticeMatrix[i,j+1]):
 					xs = [i, i]
 					ys = [j, j+1]
-					plt.plot(xs, ys, 'r')
+					ax.plot(xs, ys, 'r')
+		#plot bots:
+		radius = 0.1
+		for bot in self.bots:
+			if bot.is_pacman():
+				circle = plt.Circle(bot.get_position(), radius, color='yellow')
+			else:
+				circle = plt.Circle(bot.get_position(), radius, color='blue')
+			ax.add_artist(circle)
 
 		plt.title('The Maze')
 		# x from -0.5 -> size_x - 0.5, y from -0.5 -> size_y - 0.5
@@ -86,18 +104,22 @@ class Environment:
 		plt.axis([-0.5, self.size[0]-0.5, -0.5, self.size[1]-0.5])
 		plt.show()
 
-	def legal_move(self, start_pos, end_pos):
+	def legal_move(self, bot, end_pos):
 		"""
 		Checks if move if legal
-		:param start_pos: where a bot starts, (x0, y0)
+		:param bot: which bot is moving, i
 		:param end_pos: where a bot will end, (x1, y1)
 		:return: True if move is legal else false
 		"""
-		# True if dist == 1 and both vertices in set
+		start_pos = self.bots[bot].get_position()
+		# True if dist == 1 and both vertices in set and no collision
 		valid_move = True
 		valid_move &= (self.dist(start_pos, end_pos) == 1)
 		valid_move &= (self.verticeMatrix[end_pos[0], end_pos[1]] == 1)
 		valid_move &= (self.verticeMatrix[start_pos[0], start_pos[1]] == 1)
+		# check collisions, TODO maybe change this(?)
+		for i in range(len(self.bots)):
+			valid_move &= (i==bot or dist(start_pos, self.bots[i].get_position()) > 0)
 		if (self.printAll):
 			print("Moving from " + str(start_pos) + " to " + str(end_pos) + " is: ")
 			if (valid_move):
@@ -106,6 +128,15 @@ class Environment:
 				print("illegal")
 		return valid_move
 	
+	def move(self, bot, end_pos):
+		"""
+		Moves bot to location
+		:param bot: which bot is moving, i
+		:param end_pos: where a bot will end, (x1, y1)
+		"""
+		self.bots[bot].move(end_pos)
+		self.occupiedVertices[bot] = end_pos
+
 	def dist(self, a, b):
 		"""
 		Gets railroad/manhatten/L1 distance
@@ -114,9 +145,6 @@ class Environment:
 		"""
 		return np.abs(a[0] - b[0]) + np.abs(a[1] - b[1])
 
-
-env = Environment(printAll=True)
+env = Environment()
 env.plot_grid()
-env.legal_move(v1, v2)
-env.legal_move(v1, v3)
-env.legal_move(v1, v4)
+
